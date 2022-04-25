ffd396bfe2738231feea5decd14b1f557144081b
Chitsy18 <97879857+Chitsy18@users.noreply.github.com>
2022-04-25 11:18:10 +0530

    Update models.py

--- Ebuy/shop/models.py
+++ Ebuy/shop/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 # Create your models here.
 class Product(models.Model):
     product_id=models.AutoField
@@ -23,11 +22,8 @@ class Contact(models.Model):
     email = models.CharField(max_length=70, default="")
     phone = models.CharField(max_length=70, default="")
     desc = models.CharField(max_length=500, default="")
-
-
     def __str__(self):
         return self.name
-
 class Orders(models.Model):
     order_id = models.AutoField(primary_key=True)
     items_json = models.CharField(max_length=5000)
