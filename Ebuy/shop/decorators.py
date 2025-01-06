474e488005511be187b455d55f7d4bffe1f85a27
soham shrikant manjrekar <91745711+sohammanjrekar@users.noreply.github.com>
2025-01-06 15:24:08 +0530

    upload

--- Ebuy/shop/decorators.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import redirect
-
-def login_required(function):
-    def wrapper(request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return redirect('login')  # Redirect to the login page if the user is not authenticated
-        return function(request, *args, **kwargs)
-    return wrapper
